e8129d14ff7d9a269bd7710f47da842b1b918042
mustafa <mltm0204@gmail.com>
2022-01-08 18:24:01 +0300

    blog form and profile files are created

---
+++ src/components/BlogForm.js
@@ -0,0 +1,55 @@
+/** @format */
+
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Avatar, Button } from "@mui/material";
+import blog from "../assets/blogpost.jpeg";
+
+const BlogForm=()=>{
+  return (
+    <div className="newBlog">
+      <div className="avatar">
+        <Avatar sx={{ height: 200, width: 200 }} alt='blog' src={blog} />
+        <h2>New Blog</h2>
+      </div>
+      <div>
+        <Box
+          component='form'
+          sx={{
+            "& > :not(style)": { m: 2, width: "50ch", display: 'flex' },
+          }}
+          noValidate
+          autoComplete='off'>
+          
+            <TextField
+            required
+              id='outlined-basic'
+              label='Title'
+              variant='outlined'
+            />
+          
+          
+            <TextField
+            required
+              id='outlined-basic'
+              label='Image URL'
+              variant='outlined'
+            />
+         
+        
+            <TextField
+                required
+              id='outlined-multiline-static'
+              label='Multiline'
+              multiline
+              rows={12}
+              defaultValue='Content*'
+            />
+            <Button variant="contained" size="large">SUBMIT</Button>
+        </Box>
+      </div>
+    </div>
+  );
+}
+export default BlogForm
--- src/pages/NewBlog.js
+++ src/pages/NewBlog.js
@@ -1,54 +1,10 @@
-/** @format */
+import React from 'react'
+import BlogForm from '../components/BlogForm' 
 
-import * as React from "react";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import { Avatar, Button } from "@mui/material";
-import blog from "../assets/blogpost.jpeg";
-
-export default function BasicTextFields() {
-  return (
-    <div className="newBlog">
-      <div className="avatar">
-        <Avatar sx={{ height: 200, width: 200 }} alt='blog' src={blog} />
-        <h2>New Blog</h2>
-      </div>
-      <div>
-        <Box
-          component='form'
-          sx={{
-            "& > :not(style)": { m: 2, width: "50ch", display: 'flex' },
-          }}
-          noValidate
-          autoComplete='off'>
-          
-            <TextField
-            required
-              id='outlined-basic'
-              label='Title'
-              variant='outlined'
-            />
-          
-          
-            <TextField
-            required
-              id='outlined-basic'
-              label='Image URL'
-              variant='outlined'
-            />
-         
-        
-            <TextField
-                required
-              id='outlined-multiline-static'
-              label='Multiline'
-              multiline
-              rows={12}
-              defaultValue='Content*'
-            />
-            <Button variant="contained" size="large">SUBMIT</Button>
-        </Box>
-      </div>
-    </div>
-  );
+const NewBlog = () => {
+    return (
+        <BlogForm/>
+    )
 }
+
+export default NewBlog
