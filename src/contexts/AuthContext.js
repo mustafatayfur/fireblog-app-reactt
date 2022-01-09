bd739ab415a2e68df748e3a84e60dab8f3ecb9ea
mustafa <mltm0204@gmail.com>
2022-01-09 16:15:52 +0300

    authcontext is created

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,1 +1,14 @@
+import { createContext, useContext, useEffect, useState } from "react";
 
+export const AuthContext = createContext();
+
+export function useAuthContext() {
+    return useContext(AuthContext)
+}
+const AuthContextProvider =({children})=> {
+    return( 
+    <AuthContext.Provider>
+    {children}
+    </AuthContext.Provider>)
+}
+export default AuthContextProvider;
