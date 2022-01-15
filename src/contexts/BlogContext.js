d35924b7848258f65d7e3a4cf706eaac63e6547f
mustafa <mltm0204@gmail.com>
2022-01-15 17:53:41 +0300

    ...

--- src/contexts/BlogContext.js
+++ src/contexts/BlogContext.js
@@ -16,24 +16,24 @@ const BlogContextProvider = (props) => {
     }, [])
   
     
-    console.log(data)
+  //   console.log(data)
    
-     const getData= ()=> {
-      data.map((doc)=>{
-        const { _document } = doc
-        // console.log("doc",doc)
+  //    const getData= ()=> {
+  //     data.map((doc)=>{
+  //       const { _document } = doc
+  //       // console.log("doc",doc)
       
-        const items = _document.data.value.mapValue.fields 
-        // console.log(items)
-        const { content, get_like_count, image, published_date, title} = items
-        setTitle(title)
-        const slicedDate = published_date.timestampValue.slice(0,10)
+  //       const items = _document.data.value.mapValue.fields 
+  //       // console.log(items)
+  //       const { content, get_like_count, image, published_date, title} = items
+  //       setTitle(title)
+  //       const slicedDate = published_date.timestampValue.slice(0,10)
        
-      })
-     } 
+  //     })
+  //    } 
      
-     getData()
-   console.log(title)
+  //    getData()
+  //  console.log(title)
 
 
   return (
