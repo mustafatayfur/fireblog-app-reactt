ca1c2165f3881da794c34935387730c2fd0e5fc9
mustafa <mltm0204@gmail.com>
2022-01-16 16:53:05 +0300

    ...

--- src/app-router/AppRouter.js
+++ src/app-router/AppRouter.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from '../components/Navbar'
-import Login from '../pages/Login'
+import Login from '../pages/LoginRegister'
 import Dashboard from '../pages/Dashboard'
 import PrivateRouter from './PrivateRouter'
 import About from '../pages/About'
@@ -21,7 +21,7 @@ const AppRouter = () => {
                 <Route path="/" element={<Dashboard/>} />
                 <Route path="/login" element={<Login/>} />
                 <Route path="/register" element={<Register/>} />
-                
+
                 <Route path="/about" element={<About/>} />
                 <Route path="/details/:id" element={<Details/>} />
                 <Route path="/new-blog" element={<NewBlog/>} />
